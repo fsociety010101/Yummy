c50aa1258669934fa3172712b60af623063d235b
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-09-23 21:03:47 +0200

    added remote image loader to the cell view

---
+++ Yummy/Views/Image/RemoteImage.swift
@@ -0,0 +1,47 @@
+//
+//  RemoteImage.swift
+//  Yummy
+//
+//  Created by Dmytro Nimchynskyi on 23/09/2023.
+//
+
+import SwiftUI
+
+final class ImageLoader: ObservableObject {
+    
+    @Published var image: Image? = nil
+   
+    func load(fromURLStrinf urlString: String) {
+        NetworkManager.shared.downloadImage(fromURLString: urlString) { uiImage in
+            guard let uiImage = uiImage else { return }
+            DispatchQueue.main.async{
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+    }
+}
+
+
+struct RemoteImage: View {
+    
+    var image: Image?
+    
+    var body: some View {
+        image?.resizable() ?? Image("food-placeholder").resizable()
+    }
+}
+
+
+// used in ListView
+struct YummyRemoteImage: View {
+    
+    @StateObject var imageLoader = ImageLoader()
+    let urlString: String
+    
+    var body: some View {
+        RemoteImage(image: imageLoader.image)
+            .onAppear {
+                imageLoader.load(fromURLStrinf: urlString)
+            }
+    }
+}
