20c3e721fc33e61ad9e3fd50da7dcb16ecbfaa31
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-09-23 11:22:46 +0200

    Initial Commit

---
+++ Yummy/YummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummyApp.swift
+//  Yummy
+//
+//  Created by Dmytro Nimchynskyi on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct YummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
