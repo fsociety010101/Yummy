e97b256cadb9f376ef4c4cf504b612bf13f4f218
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-09-23 13:27:20 +0200

    added network manager

---
+++ Yummy/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Yummy
+//
+//  Created by Dmytro Nimchynskyi on 23/09/2023.
+//
+
+import Foundation
---
+++ Yummy/YummyError.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkError.swift
+//  Yummy
+//
+//  Created by Dmytro Nimchynskyi on 23/09/2023.
+//
+
+import Foundation
+
+enum YummyError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
