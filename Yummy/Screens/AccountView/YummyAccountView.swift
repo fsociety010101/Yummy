ec5d797594fa1db2f07e41de4d71655fcbbdab76
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-09-24 13:10:18 +0200

    added account info form

--- Yummy/Screens/AccountView/YummyAccountView.swift
+++ Yummy/Screens/AccountView/YummyAccountView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct YummyAccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefils = false
+    
     var body: some View {
         NavigationView {
-            Text("Account View")
-                .navigationTitle("👽 Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email Address", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                        .autocorrectionDisabled()
+                    DatePicker("Date of Birth", selection: $birthdate, displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refils", isOn: $frequentRefils)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: Color("brandPrimary")))
+                    
+            }
+            .navigationTitle("👽 Account")
         }
     }
 }
