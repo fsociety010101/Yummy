514423d789cbeaa9608cdb98c951e173006ceadb
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-09-25 18:56:46 +0200

    added regex builder
    added in ios 15/swift 5.7

--- Yummy/Utilities/Extensions/StringExtension.swift
+++ Yummy/Utilities/Extensions/StringExtension.swift
@@ -6,12 +6,45 @@
 //
 
 import Foundation
+import RegexBuilder
 
 extension String {
     
     var isValidEmail: Bool {
-        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailFormat)
-        return emailPredicate.evaluate(with: self)
+        /// old way without regex builder
+//        let emailFormat = /[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}/
+//        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailFormat)
+//        return emailPredicate.evaluate(with: self)
+        
+        
+        /// new way with regex builder
+        let emailRegex = Regex {
+            OneOrMore {
+                CharacterClass(
+                    .anyOf("._%+-"),
+                    ("A"..."Z"),
+                    ("0"..."9"),
+                    ("a"..."z")
+                )
+            }
+            "@"
+            OneOrMore {
+                CharacterClass(
+                    .anyOf(".-"),
+                    ("A"..."Z"),
+                    ("a"..."z"),
+                    ("0"..."9")
+                )
+            }
+            "."
+            Repeat(2...64) {
+                CharacterClass(
+                    ("A"..."Z"),
+                    ("a"..."z")
+                )
+            }
+        }
+        
+        return self.wholeMatch(of: emailRegex) != nil
     }
 }
